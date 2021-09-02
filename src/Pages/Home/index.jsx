ccab42d4f5de50881e7b532560460c33eae326fa
TanLocIT200600 <77155886+TanLocIT200600@users.noreply.github.com>
2021-09-02 15:28:54 +0700

    1528

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,35 +1,39 @@
-import React, { Component } from "react";
-import { fetchBanners } from "../../Store/actions/banner";
-import { connect } from "react-redux";
-import { Carousel,Typography  } from 'antd';
+import React, { useEffect,memo, Fragment } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { actionTypes } from '../../Store/actions/type';
 import './style.css'
+import { Carousel } from 'antd';
 
-class Home extends Component {
-    
-    render() {
+const Home = () => {
 
-        return (
-            <div>
-                <Carousel autoplay>
-                    {this.props.bannerList.content?.map((item) => (
-                        <img className="imgBanner" src={item.hinhAnh} alt={item.maBanner} />
-                    ))}
-                </Carousel>
-                <h1>MOVIE SELECTION</h1>
-                
+    const dispatch = useDispatch();
+    const bannerList = useSelector((state) => state.banner.bannerList)
 
-            </div>
-        );
-    }
-    componentDidMount() {
-        this.props.dispatch(fetchBanners);
-    }
+    useEffect(() =>{
+        axios({
+            method:'GET',
+            url:'https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachBanner',
+            headers:{
+                TokenCybersoft:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNjQiLCJIZXRIYW5TdHJpbmciOiIyMS8wMS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NDI3MjMyMDAwMDAiLCJuYmYiOjE2MTYxNzMyMDAsImV4cCI6MTY0Mjg3MDgwMH0.2sSWVGy-3Ce9iJ8bIYmYOJ9aE1eu3fz07DtA2ECfiyk',
+            }
+        }).then((res) =>{
+            dispatch({type:actionTypes.SET_BANNERS,payload:res.data});
+        })
+        .catch((err) => console.log(err));
+    },[]);
+
+    return (
+        <Fragment>
+            <Carousel autoplay>
+            {bannerList.content?.map((item) =>(
+                <img className="imgBanner" src={item.hinhAnh} alt={item.maBanner} />
+            ))}
+            </Carousel>
+            
+        </Fragment>
+    )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        bannerList: state.banner.bannerList,
-    };
-};
+export default memo(Home)
 
-export default connect(mapStateToProps)(Home);
---
+++ src/Store/actions/movie.js
@@ -0,0 +1,10 @@
+import { actionTypes } from "./type";
+import { createAction } from "./index";
+import { request } from "../../api/request";
+
+//async action
+export const fetchMovies = () =>{
+    request({
+        
+    })
+}
