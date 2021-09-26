c4d1644f7914a18bafc59d5621fe3f77e730d498
TanLocIT200600 <77155886+TanLocIT200600@users.noreply.github.com>
2021-09-26 21:02:16 +0700

    multi

--- src/Components/RSlick/MultipleRowSlick.jsx
+++ src/Components/RSlick/MultipleRowSlick.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
-import { useSelector, useDispatch } from 'react-redux';
-import { Button, Typography } from "antd";
-import { SET_FILM_DANG_CHIEU, SET_FILM_SAP_CHIEU } from '../../Store/constants/movieConstants'
+import { useSelector, useDispatch } from "react-redux";
+import {
+  SET_FILM_DANG_CHIEU,
+  SET_FILM_SAP_CHIEU,
+} from "../../Store/constants/movieConstants";
 import { NavLink } from "react-router-dom";
-import './MultipleRowSlick.module.css';
-import styleSlick from './MultipleRowSlick.module.css';
-import { PlayCircleOutlined } from '@ant-design/icons'
-import { Modal } from "antd";
-import "./style.css"
-import ReactPlayer from 'react-player'
-
-
+import "./MultipleRowSlick.module.css";
+import styleSlick from "./MultipleRowSlick.module.css";
+import { PlayCircleOutlined } from "@ant-design/icons";
+import "./style.css";
+import { Modal, Button } from 'antd';
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
-      className={`${className} ${styleSlick['slick-prev']}`}
+      className={`${className} ${styleSlick["slick-prev"]}`}
       style={{ ...style, display: "block" }}
       onClick={onClick}
     />
@@ -28,8 +27,8 @@ function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
-      className={`${className} ${styleSlick['slick-prev']}`}
-      style={{ ...style, display: "block", left: '-50px' }}
+      className={`${className} ${styleSlick["slick-prev"]}`}
+      style={{ ...style, display: "block", left: "-50px" }}
       onClick={onClick}
     />
   );
@@ -38,22 +37,9 @@ function SamplePrevArrow(props) {
 export default function MultipleRowSlick(props) {
   const dispatch = useDispatch();
   const arrFilm = useSelector((state) => state.movieReducers.arrFilm);
-  const { dangChieu, sapChieu } = useSelector((state) => state.movieReducers.arrFilm);
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
+  const { dangChieu, sapChieu } = useSelector(
+    (state) => state.movieReducers.arrFilm
+  );
 
   const settings = {
     className: "center variable-width",
@@ -64,42 +50,53 @@ export default function MultipleRowSlick(props) {
     speed: 500,
     rows: 2,
     nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
+    prevArrow: <SamplePrevArrow />,
   };
-  console.log('arrFilm', arrFilm);
-  const { Text } = Typography;
-  let phimDangChieu = dangChieu === true ? 'active_Film' : 'none_active_Film';
-  let phimSapChieu = sapChieu === true ? 'active_Film' : 'none_active_Film';
+  console.log("arrFilm", arrFilm);
+  let phimDangChieu = dangChieu === true ? "active_Film" : "none_active_Film";
+  let phimSapChieu = sapChieu === true ? "active_Film" : "none_active_Film";
+
+  const [visible, setVisible] = useState(false);
 
   return (
     <div>
-      <Button className={`${styleSlick[phimDangChieu]}`} style={{ marginRight: 10, marginBottom: 10 }} type="primary" danger onClick={() => {
-        const action = { type: SET_FILM_DANG_CHIEU }
-        dispatch(action);
-      }}>Phim Đang chiếu</Button>
-      <Button className={`${styleSlick[phimSapChieu]}`} type="primary" danger onClick={() => {
-        const action = { type: SET_FILM_SAP_CHIEU }
-        dispatch(action);
-      }}>Phim Sắp chiếu</Button>
+      <Button
+        className={`${styleSlick[phimDangChieu]}`}
+        style={{ marginRight: 10, marginBottom: 10 }}
+        type="primary"
+        danger
+        onClick={() => {
+          const action = { type: SET_FILM_DANG_CHIEU };
+          dispatch(action);
+        }}
+      >
+        Phim Đang chiếu
+      </Button>
+      <Button
+        className={`${styleSlick[phimSapChieu]}`}
+        type="primary"
+        danger
+        onClick={() => {
+          const action = { type: SET_FILM_SAP_CHIEU };
+          dispatch(action);
+        }}
+      >
+        Phim Sắp chiếu
+      </Button>
       <Slider {...settings}>
         {arrFilm ? arrFilm.slice(0, 20).map((phim, index) => {
           return (
             <div key={index} className="flip-card px-2">
               <div className="flip-card-front" style={{ background: `url(${phim.hinhAnh})` }}>
-                <img
-                  src={phim.hinhAnh}
-                  className="opacity-0 w-full imd-detail-mul"
-                  alt="Avatar"
-                />
+                <img src={phim.hinhAnh} className="opacity-0 w-full imd-detail-mul" alt="Avatar" />
                 <div className="movie-detail-mul"></div>
                 <div className="movie-trailer-mul">
-                  <button type="button" data-toggle="modal" data-target="#exampleModal">
+                  <button onClick={() => setVisible(true)}>
                     <PlayCircleOutlined className="play-video-mul" />
                   </button>
-                  <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
+                  <Modal title={phim.tenPhim} centered visible={visible} onOk={() => setVisible(false)} onCancel={() => setVisible(false)} width={700}
+                  footer={null}>
+                    <iframe src={phim.trailer} height="500" width="100%" allow="autoplay" frameBorder="0"></iframe>
                   </Modal>
                 </div>
               </div>
@@ -111,7 +108,8 @@ export default function MultipleRowSlick(props) {
               {phim.hot ? <button className="btn_hot">HOT</button> : <></>}
             </div>
           );
-        }) : 'none'}
+        })
+          : "none"}
       </Slider>
     </div>
   );
